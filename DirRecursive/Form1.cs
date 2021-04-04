e97ad6c9baea642cbeb2b1a0afa057f306f3d106
Kaique Moreira <kaiquemoreiracloud@gmail.com>
2021-04-04 11:46:06 -0300

    Formulario pré formatado

---
+++ DirRecursive/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DirRecursive
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //var entradas = GetFilesRecursive(@"C:\temp\PAYMENT"); // declaracao de array
+            /*foreach (string pathProduction in entradas) // dentro de entrada terá diversas entradas
+              {
+                listBox1.Items.Add(pathProduction);
+                MessageBox.Show("ARQUIVO COPIADO!");*
+               }*/
+            var prdFile = "ABCDEFGHIJK123456789";
+            var prdFileExtension = prdFile + ".prd.enc";
+
+            foreach (var file in FileUtil.GetFiles(@"C:\temp\PAYMENT\", prdFileExtension)) WriteLine(file);
+        }
+        public static class FileUtil
+        {
+            public static IEnumerable<string> GetFiles(string root, string searchPattern)
+            {
+                var pending = new Stack<string>();
+                pending.Push(root);
+                while (pending.Count != 0)
+                {
+                    var path = pending.Pop();
+                    string[] next = null;
+                    try
+                    {
+                        next = Directory.GetFiles(path, searchPattern);
+                    }
+                    catch { }
+                    if (next != null && next.Length != 0) foreach (var file in next) yield return file;
+                    try
+                    {
+                        next = Directory.GetDirectories(path);
+                        foreach (var subdir in next) pending.Push(subdir);
+                    }
+                    catch { }
+                }
+            }
+        }
+        private void WriteLine(string file)
+        {
+            DateTime DataAtual = DateTime.Now;
+
+            if (file != null)
+            {
+                string prd = Path.GetFileName(file);
+                string targetPath = @"C:\temp\AUTO_ENC\";
+
+                File.Copy(prd, targetPath);
+                //MessageBox.Show($"Arquivo {prd} Copiado com sucesso \npara {targetPath}");
+
+                System.IO.StreamWriter logFiles = new System.IO.StreamWriter(@"C:\temp\ImportProductionFiles\Log\" + "DownloadedFiles_" + DataAtual.ToString("MMddyyyy") + ".txt", true);
+                string dataimportacao = file + " - Arquivo encontrado e copiado às " + DataAtual.ToString("HH:mm:ss");
+                string savedPathFiles = String.Format(dataimportacao);
+                logFiles.WriteLine(savedPathFiles);
+                logFiles.WriteLine("--------------------------------------------------------------");
+                logFiles.Close();
+            }
+            else
+            {
+                MessageBox.Show("ARQUIVO NÃO ENCONTRADO NA REDE!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
